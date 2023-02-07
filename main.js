102f92837aab8e79bf9fdda22a4437b740cd27cb
Nyamosiyego <edmond.nyamosi@strathmore.edu>
2023-02-07 23:45:18 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm}}
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?/`, config)
+    makeImages(res.data)
+    form.elements.query.value=" ";
+    
+});
+
+const makeImages = (shows) =>{
+    for(let result of shows){
+        if(result.show.image){
+    const img = document.createElement('IMG')
+    img.src = result.show.image.medium;
+    document.body.append(img)
+    }}
+}  
+
+const deleteImages = () => {
+    const deleteImg = document.querySelectorAll("img")
+    for (let imgs of deleteImg) {
+        imgs.remove();
+        }
+    }
+
+form.addEventListener('submit', deleteImages());
